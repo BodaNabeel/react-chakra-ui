5d515353850a6c1f99526b75fcc99e686be29a80
Nabeel Boda <needtoexplore01@gmail.com>
2022-04-12 14:04:26 +0530

    useToast of ChakraUI used

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { useToast } from "@chakra-ui/react";
+
+
 function App() {
+  // using chakra ui to create toast
+  const toast = useToast();
+  const ShowToast = () => {
+    return(
+  <button
+        onClick={() => {
+          toast({
+            title: "Toast Added",
+            description: "Toast deployed successfully",
+            status: "info",
+            duration: 1000,
+            isClosable: true,
+          });
+        }}
+      >
+        Show Toast
+      </button>
+    )
+  }
   return (
-   <div>working</div>
+    <ShowToast/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
+  <React.StrictMode>
+    <ChakraProvider resetCSS={false}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
